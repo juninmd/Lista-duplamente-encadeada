e02ca29f46bf22da8b95c885dc9ad86210a718bc
Antonio Carlos do Nascimento Junior <jr_acn@yahoo.com.br>
2016-05-04 20:14:00 -0300

    incluindo malloc

--- main.c
+++ main.c
@@ -5,6 +5,9 @@
 // Inserir Inicio
 // Imprimir da esquerda para direita
 // Imprimir da direita para esquerda
+// Inserir no final
+// Buscar no elemento e retornar o ponteiro para o elemento ou null caso não encontre
+// Função que remove o ultimo elemento
 struct no {
 	int info;
 	struct no *esq, *dir;
@@ -20,6 +23,7 @@ void menuPrincipal(){
 	printf("1) Inserir Inicio\n");
 	printf("2) Listar\n");
 	printf("3) Listar reverso\n");
+	printf("4) Inserir no final\n");
 	printf("\n9) Sair\n\n");
 }
 
@@ -47,6 +51,7 @@ void insere_inicio(struct no **lista){
 	
 }
 
+//2
 void imprimir(struct no *lista){
 	struct no *p;
 	p = lista;
@@ -58,6 +63,7 @@ void imprimir(struct no *lista){
 	getch();
 } 
 
+//3
 void imprimirReverso(struct no *lista){
 	struct no *p,*q;
 	p = lista;
@@ -76,6 +82,37 @@ void imprimirReverso(struct no *lista){
 	}
 	getch();
 } 
+
+// - 4
+void inserir_final(struct no **lista){
+	int x = 0;
+	printf("Insira o valor:");
+	scanf("%d",&x);
+		
+	struct no *p;
+	struct no *q;
+	struct no *r;
+	q = *lista;
+
+	if((p=malloc(sizeof(struct no))) == NULL || (r=malloc(sizeof(struct no))) == NULL){
+		printf("Erro na alocação de memória");
+	}
+	else{
+		
+		while(q != NULL){
+			if(q->esq == NULL){
+				p = q;
+			}
+			q = q->esq;
+		}
+		r->info = x;
+		r->esq = q;
+		r->dir = NULL;
+
+		*lista= p;
+	}
+	
+}
 
 
 int main(int argc, char *argv[]) {
